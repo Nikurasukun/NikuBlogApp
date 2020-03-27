d3cacd49a4bde3e93690f548d87947ede0f9ad9b
Nikurasukun <nikurasu@vivaldi.net>
2020-03-27 14:51:55 +0100

    Navigation mit Zurück-Taste/Geste/was auch immer hinzugefügt

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ So, damit man auch von den mobilen Endgeräten bequemen Zugriff auf meinen Blog 
 Bisher hab ich ja schon eine in den Grundzügen funktionierende App, sie kann:
 <ul>
   <li>Die mobile Version der Seite aufrufen</li>
-  <li>Öffnet nur seiteninterne Links in der App, bei anderen, wie z.B. meiner Twitter-Verlinkung wird gefragt ob der Link in der Twitter App oder in eurem Browser geöffnet werden soll.
+  <li>Öffnet nur seiteninterne Links in der App, bei anderen, wie z.B. meiner Twitter-Verlinkung wird gefragt ob der Link in der Twitter App oder in eurem Browser geöffnet werden soll.</li>
+  <li>Man kann mit der Zurück-/Taste/Geste/wie auch immer durch die Chronik navigieren.</li>
 </ul>
 
 Aber ich hab auch schon einige geplante Funktionen, die wären:
 <ul>
-  	<li>Man kann mit der Zurück-/Taste/Geste/wie auch immer durch die Chronik navigieren. (Nächstes Ziel)</li>
   	<li>Das öffnen der Links in Custom Tabs statt im Browser</li>
 </ul>
--- app/src/main/java/xyz/nikurasu/nikublog/MainActivity.kt
+++ app/src/main/java/xyz/nikurasu/nikublog/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -37,7 +38,18 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         }
-        }
     }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Check if the key event was the Back button and if there's history
+        val myWebView: WebView = findViewById(R.id.webview)
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event)
+    }
+}
 
 
